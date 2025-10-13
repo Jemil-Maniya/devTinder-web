525dfb01da0838c007302ea45f8e8404558f4e2b
Jemil <jemilmaniya27@gmail.com>
2025-10-13 15:59:51 +0530

    fxed ijmports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Body from "./Body";
-import Login from "./Login";
-import Profile from "./Profile";
-
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   return (
@@ -10,8 +9,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Body />}>
-            <Route path="login" element= {<Login />} />
-            <Route path="profile" element = {<Profile />} />
+            <Route path="login" element={<Login />} />
+            <Route path="profile" element={<Profile />} />
           </Route>
         </Routes>
       </BrowserRouter>
